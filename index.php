a262ebfa55217e8736d4941a9eb469742de05107
Dan OBoyle <oboyledk@gmail.com>
2012-02-02 00:10:18 -0500

    third

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include "includes/password_protect.php";?>
+<?php include "includes/abovecss.php";?>
+<head>
+<?php include "includes/css.php";?>
+
+
+</head>
+<?php include"includes/css2.php"; 
+?>
+
+<Body>
+
+<center><h1>VStream</h1></center>
+<center><a href="TVShows.php" class="nice radius blue button">Enter Site </a></center>
+<center><a href="TVShows.php"><img src="/images/lue.jpg"></a></Center>
+<br>
+
+</Body>
+
+</body>
+</html>
